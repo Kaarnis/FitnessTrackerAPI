97118117b1203ed047b08fdae711c29ede4be35f
kaarnis <karnarantaveeti@gmail.com>
2024-03-31 01:26:06 +0200

    Allow cors also for authController

--- src/main/java/com/karnisdev/fitnesstrackerapi/controller/AuthController.java
+++ src/main/java/com/karnisdev/fitnesstrackerapi/controller/AuthController.java
@@ -8,6 +8,7 @@ import com.karnisdev.fitnesstrackerapi.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ import java.net.URI;
 
 @RequiredArgsConstructor
 @RestController
-//@RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
     private final UserService userService;
